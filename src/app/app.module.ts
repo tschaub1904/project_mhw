455efc75d6eaef72ce7e12b10cddd17ee87fd62a
Adrian J <godeatingtree@gmail.com>
2020-02-07 18:44:41 +0100

    Added layout footer, header and main-area component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { MainAreaComponent } from './components/layout/main-area/main-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainAreaComponent
   ],
   imports: [
     BrowserModule
