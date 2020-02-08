4dce1909714ef4f5fff14224ecb45b440197a9c4
Adrian J <godeatingtree@gmail.com>
2020-02-08 12:58:48 +0100

    Footer, Header, Main area adjusted, search-bar and sidebar added as component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
 import { MainAreaComponent } from './components/layout/main-area/main-area.component';
+import { SidebarComponent } from './components/layout/sidebar/sidebar.component';
+import { SearchBarComponent } from './components/layout/search-bar/search-bar.component';
+
+
+const routes: Routes = [{path: '', component: MainAreaComponent}]
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MainAreaComponent
+    MainAreaComponent,
+    SidebarComponent,
+    SearchBarComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
