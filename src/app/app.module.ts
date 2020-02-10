3efb76cc9f4d348850666eb16215274ebba5beee
Tobias Schaub <schaubi93@gmail.com>
2020-02-10 11:16:54 +0100

    added missing ','

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [{path: '', component: MainAreaComponent}]
     
   ],
   imports: [
-    HttpClientModule
+    HttpClientModule,
     BrowserModule,
     RouterModule.forRoot(routes),
   ],
