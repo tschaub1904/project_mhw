2dbccdebf50b23fff91fea3837b6b8eff23cec2e
Adrian J <godeatingtree@gmail.com>
2020-02-10 13:17:53 +0100

    renamed skill.ts to decoration.ts

--- src/app/services/decorations/decorations.service.ts
+++ src/app/services/decorations/decorations.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Decoration } from 'src/app/models/skills';
+import { Decoration } from 'src/app/models/decoration';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
