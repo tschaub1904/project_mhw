1c4a7095ae7457d7f37700e7ab665305249b5fe0
Tobias Schaub <schaubi93@gmail.com>
2020-03-27 14:22:19 +0100

    changed loadout tile style
    changed equipment style
    changed itemlist style

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -12,6 +12,7 @@ export class HeaderComponent implements OnInit {
   constructor(private tileFactory: TileFactoryService, private to: TileOrganizer) { }
 
   ngOnInit() {
+    this.openLoadoutTile();
   }
 
   openLoadoutTile() {
--- src/app/components/loadout/loadout.component.ts
+++ src/app/components/loadout/loadout.component.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit, ContentChildren, QueryList, ContentChild, ElementRef } from "@angular/core";
+import { Component, OnInit, HostListener, ViewChild, ElementRef, AfterViewInit, OnDestroy } from "@angular/core";
 import { TileFactoryService } from "src/app/services/tileFactory.service";
 import { EquipmentLoadout } from "src/app/classes/equipmentLoadout.class";
+import { ItemListComponent } from '../item-list/item-list.component';
 
 @Component({
 	selector: "app-loadout",
 	templateUrl: "./loadout.component.html",
 	styleUrls: ["./loadout.component.sass"]
 })
-export class LoadoutComponent implements OnInit {
+export class LoadoutComponent implements OnInit, AfterViewInit, OnDestroy {
 	loadout: EquipmentLoadout = new EquipmentLoadout();
 	items: any[] = [];
 	type: string;
 	active: boolean = false;
+	@ViewChild('picker', { static: false }) picker: ElementRef;
 
 	constructor(private tileFactory: TileFactoryService) { }
 
 	ngOnInit() { }
+	ngAfterViewInit() { }
+	ngOnDestroy() { }
 
 	showList(type: string, subtype?: string) {
 
@@ -29,11 +33,21 @@ export class LoadoutComponent implements OnInit {
 		this.active = true;
 	}
 
-	setSlot(event: any){
-		console.log("setSLot ",event, this.type)
+	setSlot(event: any) {
+		console.log("setSLot ", event, this.type)
 		this.loadout.setItem(event, this.type);
 		this.loadout.printLoadout();
 
 		this.active = false;
-  }
+	}
+
+	@HostListener('click', ["$event"])
+	mouseClicked(e: MouseEvent) {
+		let { x, y } = e;
+		console.log(e);
+		this.picker.nativeElement.style.left = x + "px";
+		this.picker.nativeElement.style.top = y + "px";
+
+	}
+
 }
